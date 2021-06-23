5f99eb9b1685f90992d6ad5eb1915a8ca550e053
MindFreeze <freonblood@gmail.com>
2021-06-23 18:50:30 +0300

    use simpler config

--- monitor.py
+++ monitor.py
@@ -17,19 +17,19 @@ client = mqtt.Client(client_id=os.environ['MQTT_CLIENT_ID'])
 client.username_pw_set(os.environ['MQTT_USER'], os.environ['MQTT_PASS'])
 client.connect(os.environ['MQTT_SERVER'])
 
-BASE_TOPIC = os.environ['MQTT_TOPIC_DISCOVERY'] + '/sensor/'
-STATE_TOPIC = BASE_TOPIC + os.environ['MQTT_TOPIC']
-CELLS_TOPIC = BASE_TOPIC + os.environ['MQTT_TOPIC_CELLS']
 devId = os.environ['DEVICE_ID']
+BASE_TOPIC = os.environ['MQTT_DISCOVERY_PREFIX'] + '/sensor/'
+STATE_TOPIC = BASE_TOPIC + devId
 deviceConf = '"device": {"manufacturer": "Dongfuan Daly Electronics", "name": "Smart BMS", "identifiers": ["' + devId + '"]}'
 # publish MQTT Discovery configs to Home Assistant
-socHaConf = '{"device_class": "battery", "name": "LiFePo SOC", "state_topic": "' + STATE_TOPIC +'/state", "unit_of_measurement": "%", "value_template": "{{ value_json.soc}}", "unique_id": "' + devId + '_soc", ' + deviceConf + '}' 
-client.publish(STATE_TOPIC +'/config', socHaConf, 0, True)
-voltageHaConf = '{"device_class": "voltage", "name": "LiFePo Voltage", "state_topic": "' + STATE_TOPIC +'/state", "unit_of_measurement": "V", "value_template": "{{ value_json.voltage}}", "unique_id": "' + devId + '_voltage", ' + deviceConf + '}' 
-client.publish(BASE_TOPIC + 'lifepoV/config', voltageHaConf, 0, True)
-currentHaConf = '{"device_class": "current", "name": "LiFePo Current", "state_topic": "' + STATE_TOPIC +'/state", "unit_of_measurement": "A", "value_template": "{{ value_json.current}}", "unique_id": "' + devId + '_current", ' + deviceConf + '}' 
-client.publish(BASE_TOPIC + 'lifepoA/config', currentHaConf, 0, True)
-cellsHaConf = '{"device_class": "voltage", "name": "LiFePo Cell Balance", "state_topic": "' + CELLS_TOPIC + '/state", "unit_of_measurement": "V", "value_template": "{{ value_json.diff}}", "json_attributes_topic": "' + CELLS_TOPIC + '/state", "unique_id": "' + devId + '_balance", ' + deviceConf + '}' 
+socHaConf = '{"device_class": "battery", "name": "Battery SOC", "state_topic": "' + STATE_TOPIC +'/state", "unit_of_measurement": "%", "value_template": "{{ value_json.soc}}", "unique_id": "' + devId + '_soc", ' + deviceConf + '}' 
+client.publish(STATE_TOPIC +'_soc/config', socHaConf, 0, True)
+voltageHaConf = '{"device_class": "voltage", "name": "Battery Voltage", "state_topic": "' + STATE_TOPIC +'/state", "unit_of_measurement": "V", "value_template": "{{ value_json.voltage}}", "unique_id": "' + devId + '_voltage", ' + deviceConf + '}' 
+client.publish(STATE_TOPIC + '_voltage/config', voltageHaConf, 0, True)
+currentHaConf = '{"device_class": "current", "name": "Battery Current", "state_topic": "' + STATE_TOPIC +'/state", "unit_of_measurement": "A", "value_template": "{{ value_json.current}}", "unique_id": "' + devId + '_current", ' + deviceConf + '}' 
+client.publish(STATE_TOPIC + '_current/config', currentHaConf, 0, True)
+CELLS_TOPIC = STATE_TOPIC + '_balance'
+cellsHaConf = '{"device_class": "voltage", "name": "Battery Cell Balance", "state_topic": "' + CELLS_TOPIC + '/state", "unit_of_measurement": "V", "value_template": "{{ value_json.diff}}", "json_attributes_topic": "' + CELLS_TOPIC + '/state", "unique_id": "' + devId + '_balance", ' + deviceConf + '}' 
 client.publish(CELLS_TOPIC + '/config', cellsHaConf, 0, True)
 
 def cmd(command):
--- run.sh
+++ run.sh
@@ -5,9 +5,7 @@ MQTT_SERVER="$(jq --raw-output '.mqtt_server' $CONFIG_PATH)" \
 MQTT_USER="$(jq --raw-output '.mqtt_user' $CONFIG_PATH)" \
 MQTT_PASS="$(jq --raw-output '.mqtt_pass' $CONFIG_PATH)" \
 MQTT_CLIENT_ID="$(jq --raw-output '.mqtt_client_id' $CONFIG_PATH)" \
-MQTT_TOPIC_DISCOVERY="$(jq --raw-output '.mqtt_topic_discovery' $CONFIG_PATH)" \
-MQTT_TOPIC_CELLS="$(jq --raw-output '.mqtt_topic_cells' $CONFIG_PATH)" \
-MQTT_TOPIC="$(jq --raw-output '.mqtt_topic' $CONFIG_PATH)" \
+MQTT_DISCOVERY_PREFIX="$(jq --raw-output '.mqtt_discovery_prefix' $CONFIG_PATH)" \
 DEVICE="$(jq --raw-output '.device' $CONFIG_PATH)" \
 DEVICE_ID="$(jq --raw-output '.device_id' $CONFIG_PATH)" \
 python3 /monitor.py
