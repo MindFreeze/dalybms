10a66b6adab80a57aa53c43721718c1abb93bac2
root <root@a0d7b954-ide.local.hass.io>
2021-10-24 18:53:59 +0300

    retain ha config in MQTT

--- monitor.py
+++ monitor.py
@@ -23,14 +23,14 @@ STATE_TOPIC = BASE_TOPIC + devId
 deviceConf = '"device": {"manufacturer": "Dongfuan Daly Electronics", "name": "Smart BMS", "identifiers": ["' + devId + '"]}'
 # publish MQTT Discovery configs to Home Assistant
 socHaConf = '{"device_class": "battery", "name": "Battery SOC", "state_topic": "' + STATE_TOPIC +'/state", "unit_of_measurement": "%", "value_template": "{{ value_json.soc}}", "unique_id": "' + devId + '_soc", ' + deviceConf + '}' 
-client.publish(STATE_TOPIC +'_soc/config', socHaConf, 0, False)
+client.publish(STATE_TOPIC +'_soc/config', socHaConf, 0, True)
 voltageHaConf = '{"device_class": "voltage", "name": "Battery Voltage", "state_topic": "' + STATE_TOPIC +'/state", "unit_of_measurement": "V", "value_template": "{{ value_json.voltage}}", "unique_id": "' + devId + '_voltage", ' + deviceConf + '}' 
-client.publish(STATE_TOPIC + '_voltage/config', voltageHaConf, 0, False)
+client.publish(STATE_TOPIC + '_voltage/config', voltageHaConf, 0, True)
 currentHaConf = '{"device_class": "current", "name": "Battery Current", "state_topic": "' + STATE_TOPIC +'/state", "unit_of_measurement": "A", "value_template": "{{ value_json.current}}", "unique_id": "' + devId + '_current", ' + deviceConf + '}' 
-client.publish(STATE_TOPIC + '_current/config', currentHaConf, 0, False)
+client.publish(STATE_TOPIC + '_current/config', currentHaConf, 0, True)
 CELLS_TOPIC = STATE_TOPIC + '_balance'
 cellsHaConf = '{"device_class": "voltage", "name": "Battery Cell Balance", "state_topic": "' + CELLS_TOPIC + '/state", "unit_of_measurement": "V", "value_template": "{{ value_json.diff}}", "json_attributes_topic": "' + CELLS_TOPIC + '/state", "unique_id": "' + devId + '_balance", ' + deviceConf + '}' 
-client.publish(CELLS_TOPIC + '/config', cellsHaConf, 0, False)
+client.publish(CELLS_TOPIC + '/config', cellsHaConf, 0, True)
 
 def cmd(command):
     res = []
